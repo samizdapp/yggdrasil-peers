15265d740f3c4ad1e8da52a4c6ef1a8c53fc2394
Ryan Bennett <your@mail.com>
2022-08-15 14:39:04 -0600

    reorder watch loop for faster bootstrap

--- watch.sh
+++ watch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 export VERSION=5
+touch "/peers/hosts_crawled$VERSION"
 while :
 do
   echo "peers crawl loop"
-  echo "run crawl script"
-  python3 -u example.py
-  echo "ran script"
   tmp=$(mktemp)
   cat hosts_header > $tmp
   cat "/peers/hosts_crawled$VERSION" >> $tmp
   cat $tmp > /peers/yg_hosts
   cat /peers/yg_hosts
+  echo "run crawl script"
+  python3 -u example.py
+  echo "ran script"
+
   sleep 10
 done
