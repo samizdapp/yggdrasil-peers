f2b57d538bbd474c227b493b2039d6e77855a0f2
Ryan Bennett <your@mail.com>
2022-10-13 12:06:13 -0600

    only update yg_hosts on diff

--- watch.sh
+++ watch.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-export VERSION=11
+export VERSION=12
 touch "/shared_etc/hosts_crawled$VERSION"
+SLEEP=10
 while :
 do
   echo "peers crawl loop"
   tmp=$(mktemp)
   cat hosts_header > $tmp
   cat "/shared_etc/hosts_crawled$VERSION" >> $tmp
-  cat $tmp > /shared_etc/yg_hosts
+
+  if diff $tmp /shared_etc/yg_hosts > /dev/null
+  then
+      echo "No difference, skip update, increase sleep"
+      SLEEP=10
+  else
+      SLEEP=10
+      cat $tmp > /shared_etc/yg_hosts
+      echo "Difference, update yg_hosts, reset sleep"
+  fi
   rm $tmp
   cat /shared_etc/yg_hosts
   echo "run crawl script"
   python3 -u example.py
   echo "ran script"
 
-  sleep 10
+  sleep $SLEEP
 done
